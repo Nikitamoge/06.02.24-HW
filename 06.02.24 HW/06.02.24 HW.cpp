674462fe7f005ef90d45dacb081eb439b3e5a06a
Nikita Iliuschchenko <Nike.il.od@gmail.com>
2024-02-05 11:49:30 +0100

    Add project files.

---
+++ 06.02.24 HW/06.02.24 HW.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <class T> 
+
+class Swapper {
+public:
+    static void Swap(T& x, T& y) 
+    {
+        T Temp = x;
+        x = y;
+        y = Temp;
+    }
+};
+
+int main()
+{
+    double a = 3.5;
+    double b = 5.5;
+
+    cout << "Before: " << a << "  |  " << b << endl;
+
+    Swapper<double>::Swap(a, b);
+
+    cout << "After: " << a << "  |  " << b << endl;
+
+}
+
